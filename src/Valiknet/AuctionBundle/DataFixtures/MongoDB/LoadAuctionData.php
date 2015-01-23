6b5c369c6a414233cde8d2c1eeec8fdf4a738894
Valik Grinevich <valik.v1per@gmail.com>
2015-01-23 19:01:05 +0200

    add fixtures

---
+++ src/Valiknet/AuctionBundle/DataFixtures/MongoDB/LoadAuctionData.php
@@ -0,0 +1,14 @@
+<?php
+namespace Valiknet\AuctionBundle\DataFixtures\MongoDB;
+
+use Hautelook\AliceBundle\Alice\DataFixtureLoader;
+
+class LoadAuctionData extends DataFixtureLoader
+{
+    public function getFixtures()
+    {
+        return [
+            __DIR__."/fixtures.yml"
+        ];
+    }
+} 
