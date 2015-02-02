781b750b6bfb282cb3f1a875a38c6a6d2347f931
Valik Grinevich <valik.v1per@gmail.com>
2015-02-02 02:41:16 +0200

    update document and fixtures

--- src/Valiknet/LotBundle/Document/FinishLot.php
+++ src/Valiknet/LotBundle/Document/FinishLot.php
@@ -1,5 +1,5 @@
 <?php
-namespace Valikent\LotBundle\Document;
+namespace Valiknet\LotBundle\Document;
 
 use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
 
@@ -35,4 +35,102 @@ class FinishLot
      * @ODM\Field(type="string")
      */
     protected $accountNewPassword;
+
+    /**
+     * Get id
+     *
+     * @return id $id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set lot
+     *
+     * @param Valiknet\LotBundle\Document\AbstractLot $lot
+     * @return self
+     */
+    public function setLot(\Valiknet\LotBundle\Document\AbstractLot $lot)
+    {
+        $this->lot = $lot;
+        return $this;
+    }
+
+    /**
+     * Get lot
+     *
+     * @return Valiknet\LotBundle\Document\AbstractLot $lot
+     */
+    public function getLot()
+    {
+        return $this->lot;
+    }
+
+    /**
+     * Set accountName
+     *
+     * @param string $accountName
+     * @return self
+     */
+    public function setAccountName($accountName)
+    {
+        $this->accountName = $accountName;
+        return $this;
+    }
+
+    /**
+     * Get accountName
+     *
+     * @return string $accountName
+     */
+    public function getAccountName()
+    {
+        return $this->accountName;
+    }
+
+    /**
+     * Set accountOldPassword
+     *
+     * @param string $accountOldPassword
+     * @return self
+     */
+    public function setAccountOldPassword($accountOldPassword)
+    {
+        $this->accountOldPassword = $accountOldPassword;
+        return $this;
+    }
+
+    /**
+     * Get accountOldPassword
+     *
+     * @return string $accountOldPassword
+     */
+    public function getAccountOldPassword()
+    {
+        return $this->accountOldPassword;
+    }
+
+    /**
+     * Set accountNewPassword
+     *
+     * @param string $accountNewPassword
+     * @return self
+     */
+    public function setAccountNewPassword($accountNewPassword)
+    {
+        $this->accountNewPassword = $accountNewPassword;
+        return $this;
+    }
+
+    /**
+     * Get accountNewPassword
+     *
+     * @return string $accountNewPassword
+     */
+    public function getAccountNewPassword()
+    {
+        return $this->accountNewPassword;
+    }
 }
